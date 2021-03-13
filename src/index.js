95c3e9da26e6e605ed4c7970bcfd86b4967304a0
Ramitsan <ramitsan@gmail.com>
2021-03-13 17:16:25 +0300

    feat: add the solution of the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+
+  const numerals = {
+    0: '',
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+    10: 'ten',
+    11: 'eleven',
+    12: 'twelve',
+    13: 'thirteen',
+    14: 'fourteen',
+    15: 'fifteen',
+    16: 'sixteen',
+    17: 'seventeen',
+    18: 'eighteen',
+    19: 'nineteen',
+    20: 'twenty',
+    30: 'thirty',
+    40: 'forty',
+    50: 'fifty',
+    60: 'sixty',
+    70: 'seventy',
+    80: 'eighty',
+    90: 'ninety'
+  }
+
+  if (number === 0) return 'zero';
+  if (number > 0 && number < 20) return numerals[number];
+
+  if (number < 100) {
+    return numerals[Math.floor(number / 10) * 10] + (number % 10 !== 0 ? ' ' + toReadable(number % 10) : '');
+  }
+  if (number >= 100) {
+    return numerals[Math.floor(number / 100)] + ' hundred' + (number % 100 !== 0 ? ' ' + toReadable(number % 100) : '');
+  }
+};
